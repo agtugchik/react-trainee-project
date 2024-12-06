1a27f60dd584ab74719f518d49fb425ea081defc
agtugchik <agtugchik13@gmail.com>
2024-12-06 18:46:39 +0300

    fix: jest issues

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -8,6 +8,7 @@ export const App = () => {
 
   return (
     <>
+      {/* {'react-trainee-project'} */}
       {appRoutes}
       <Page />
     </>
--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { screen, render, cleanup } from '@testing-library/react';
-import { App } from '../components/app';
+import { App } from 'components/app';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('App component', () => {
   beforeAll(() => {
-    render(<App />);
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
   });
 
   it('should have the right message in the dom', () => {
