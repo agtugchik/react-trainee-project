3544b58b22dfdb09bf6e9c3f762bb88a33f967aa
agtugchik <agtugchik13@gmail.com>
2024-12-10 18:29:39 +0300

    refactor: delete unnecessary state for checkbox

--- src/components/ui/remember-checkbox/remember-checkbox.tsx
+++ src/components/ui/remember-checkbox/remember-checkbox.tsx
@@ -1,5 +1,5 @@
 import { FormFieldsNames } from 'constants/form-fields-names';
-import React, { useState } from 'react';
+import React from 'react';
 import { UseFormReturn } from 'react-hook-form';
 
 type Props = {
@@ -8,7 +8,6 @@ type Props = {
 };
 
 export const RememberCheckbox = ({ form }: Props) => {
-  const [checked, setChecked] = useState(false);
   const { register } = form;
 
   return (
@@ -19,10 +18,6 @@ export const RememberCheckbox = ({ form }: Props) => {
           {...register(FormFieldsNames.REMEMBER)}
           type="checkbox"
           value=""
-          checked={checked}
-          onChange={() => {
-            setChecked(!checked);
-          }}
           className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
         />
       </div>
