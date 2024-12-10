de4d030807ba65285c655c33d106786d3c65e31c
agtugchik <agtugchik13@gmail.com>
2024-12-10 15:40:00 +0300

    refactor: account condition component

--- src/components/ui/account-condition/account-condition.tsx
+++ src/components/ui/account-condition/account-condition.tsx
@@ -11,15 +11,12 @@ export const AccountCondition = () => {
 
   return (
     <p className="mt-10 text-center text-sm/6 text-gray-500">
-      {pathname === AppPaths.AUTH && 'No accaunt? '}
-      {pathname === AppPaths.SIGNUP && 'Already have account? '}
-
+      {pathname === AppPaths.AUTH ? 'No accaunt? ' : 'Already have account?'}
       <span
         onClick={() => navigate(pathname === AppPaths.AUTH ? AppPaths.SIGNUP : AppPaths.AUTH)}
         className={className}
       >
-        {pathname === AppPaths.AUTH && 'Create now'}
-        {pathname === AppPaths.SIGNUP && 'Log In'}
+        {pathname === AppPaths.AUTH ? 'Create now' : 'Log In'}
       </span>
     </p>
   );
