0cdd77dcca5e534be403e4379082c772542a0ef7
agtugchik <agtugchik13@gmail.com>
2024-12-12 15:07:09 +0300

    feat: add init disable for submit button

--- src/components/ui/submit-button/submit-button.tsx
+++ src/components/ui/submit-button/submit-button.tsx
@@ -8,12 +8,19 @@ export const SubmitButton = ({ form }: BaseInputProps) => {
   const pathname = location.pathname;
   const {
     formState: { isDirty, isValid },
+    getValues,
   } = form;
+
+  const initButtonDisabled = () => {
+    const formValues = Object.entries(getValues());
+    const isDisabled = formValues.every((v) => !v[1].length);
+    return isDisabled;
+  };
 
   return (
     <div>
       <button
-        disabled={isDirty && !isValid}
+        disabled={(isDirty && !isValid) || initButtonDisabled()}
         type="submit"
         className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:bg-gray-300 disabled:cursor-not-allowed disabled:opacity-50"
       >
