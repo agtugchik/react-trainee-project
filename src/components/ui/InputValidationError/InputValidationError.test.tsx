3e32b2d58f28012c66e1c9f8ed1b7c6fbe273a0b
agtugchik <agtugchik13@gmail.com>
2024-12-17 17:24:06 +0300

    feat: add input validation component test

---
+++ src/components/ui/InputValidationError/InputValidationError.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { cleanup, render, screen } from '@testing-library/react';
+import { errors, InputValidationError } from '../InputValidationError/InputValidationError';
+import { FormFieldsNames } from 'constants/form-fields-names';
+
+const { getByText } = screen;
+
+describe('EmailInput component', () => {
+  afterEach(cleanup);
+
+  it('should throw email error', () => {
+    render(<InputValidationError inputType={FormFieldsNames.EMAIL} />);
+    expect(getByText(errors.email)).toBeInTheDocument();
+  });
+
+  it('should throw password error', () => {
+    render(<InputValidationError inputType={FormFieldsNames.PASSWORD} />);
+    expect(getByText(errors.password)).toBeInTheDocument();
+  });
+
+  it('should throw confirm password error', () => {
+    render(<InputValidationError inputType={FormFieldsNames.CONFIRM_PASSWORD} />);
+    expect(getByText(errors.confirm_password)).toBeInTheDocument();
+  });
+});
