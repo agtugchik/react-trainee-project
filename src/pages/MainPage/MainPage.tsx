2359571d245cde97397415097e2d5010762e4e49
agtugchik <agtugchik13@gmail.com>
2024-12-19 20:57:01 +0300

    feat: add infinite scroll

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { getPhotos } from 'api/unsplash-api';
+import { useInView } from 'react-intersection-observer';
 
 export const pageTitle = 'MainPage';
 
 export const MainPage = () => {
-  const { data, fetchNextPage } = useInfiniteQuery({
+  const { ref, inView, entry } = useInView();
+  const { data, fetchNextPage, isFetchingNextPage, hasNextPage } = useInfiniteQuery({
     queryKey: ['photos'],
     queryFn: getPhotos,
     initialPageParam: 1,
-    getNextPageParam: (lastPage, allPages, lastPageParam) => lastPageParam + 1,
+    getNextPageParam: (lastPage, allPages, lastPageParam) => {
+      if (lastPage.length === 0) return;
+      return lastPageParam + 1;
+    },
+    refetchOnWindowFocus: false,
   });
+
+  useEffect(() => {
+    if (entry && inView) fetchNextPage();
+  }, [entry, inView, fetchNextPage]);
 
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
@@ -25,7 +35,7 @@ export const MainPage = () => {
           </div>
         ))
       )}
-      <button onClick={() => fetchNextPage()}>LoadMore</button>
+      {isFetchingNextPage ? <>Loading...</> : hasNextPage && <div ref={ref} />}
     </div>
   );
 };
