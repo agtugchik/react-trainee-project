e5b19095f31e6beddef6b4f3e71f32439ad763b2
agtugchik <agtugchik13@gmail.com>
2024-12-19 19:29:13 +0300

    feat: add photos collection

--- src/api/unsplash-api.ts
+++ src/api/unsplash-api.ts
@@ -1,6 +1,17 @@
 const baseUrl = 'https://api.unsplash.com';
 
-export const getRandomPhotos = async (count = 30) => {
+interface Image {
+  id: string;
+  urls: {
+    raw: string;
+    full: string;
+    regular: string;
+    small: string;
+    thumb: string;
+  };
+}
+
+export const getRandomPhotos = async (count = 30): Promise<Image[]> => {
   const response = await fetch(
     `${baseUrl}/photos/random?client_id=${process.env.UNSPLASH_ACCESS_KEY}&count=${count}`
   );
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -5,8 +5,20 @@ import { getRandomPhotos } from 'api/unsplash-api';
 export const pageTitle = 'MainPage';
 
 export const MainPage = () => {
-  const query = useQuery({ queryKey: ['photos'], queryFn: () => getRandomPhotos() });
+  const query = useQuery({ queryKey: ['photos'], queryFn: () => getRandomPhotos(9) });
 
   useEffect(() => console.log(query.data), [query]);
-  return <div>{pageTitle}</div>;
+  return (
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+      {query.data?.map((photo) => (
+        <div key={photo.id}>
+          <img
+            className="object-cover object-center w-full h-80 max-w-full rounded-lg"
+            src={photo.urls.small}
+            alt="gallery-photo"
+          />
+        </div>
+      ))}
+    </div>
+  );
 };
