57cfdada2918a775434e28cbfbfe6d28f616034a
agtugchik <agtugchik13@gmail.com>
2024-12-23 15:48:37 +0300

    feat: hide overflow x scroll

---
+++ src/components/ui/ParamsFilter/ParamsFilter.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const ParamsFilter = () => <div></div>;
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -37,7 +37,7 @@ export const MainPage = () => {
   }, [entry, inView, fetchNextPage]);
 
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 overflow-x-hidden">
       {data?.pages.map((page) => page.map((photo) => <Photo key={photo.id} photo={photo} />))}
       {isFetchingNextPage ? <>Loading...</> : hasNextPage && <div ref={ref} />}
     </div>
