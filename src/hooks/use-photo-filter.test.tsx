3c84cb5f33772fdedb7f9b9c40f2b21dbcfae913
agtugchik <agtugchik13@gmail.com>
2024-12-26 16:22:29 +0300

    feat: add test for use photo filter hook

---
+++ src/hooks/use-photo-filter.test.tsx
@@ -0,0 +1,16 @@
+import { renderHook } from '@testing-library/react';
+import { usePhotoFilter } from './use-photo-filter';
+import { QueryProvider } from 'context/';
+
+const testName = 'testName';
+const testValue = 'testValue';
+
+describe('usePhotoFilter', () => {
+  test('should work correct', () => {
+    const { result } = renderHook(usePhotoFilter, { wrapper: QueryProvider });
+    console.log(result);
+    expect(typeof result.current).toBe('function');
+    result.current(testName, testValue);
+    expect(localStorage.getItem(testName)).toEqual(testValue);
+  });
+});
