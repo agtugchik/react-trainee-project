f0b5c6fb64f890bc899e663e20f4fcf5e2cbc978
agtugchik <agtugchik13@gmail.com>
2024-12-26 17:26:13 +0300

    refactor: main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,36 +1,47 @@
 import React, { useEffect } from 'react';
-import { useInfiniteQuery } from '@tanstack/react-query';
+import { GetNextPageParamFunction, InfiniteData, useInfiniteQuery } from '@tanstack/react-query';
 import { getPhotos } from 'api/unsplash-api';
 import { useInView } from 'react-intersection-observer';
 import { Photo } from 'components/Photo';
 import { ParamsFilter } from 'components/ui/ParamsFilter';
-import { Image, SearchResponse } from 'types/unsplash-types';
+import { Image, LikedImage, SearchResponse } from 'types/unsplash-types';
 
 export const pageTitle = 'MainPage';
 
 export const MainPage = () => {
+  const transformPhoto: (data: InfiniteData<SearchResponse | Image[], number>) => {
+    pages: LikedImage[][];
+    pageParams: number[];
+  } = (photo) => {
+    const photoWithLike = photo.pages.map((page) =>
+      ((page as SearchResponse).results || (page as Image[])).map((photo) => ({
+        ...photo,
+        isLiked: localStorage.getItem(String(photo.id)) === 'true',
+      }))
+    );
+
+    return {
+      pages: photoWithLike,
+      pageParams: photo.pageParams,
+    };
+  };
+
+  const getNextPageParam: GetNextPageParamFunction<number, SearchResponse | Image[]> = (
+    lastPage,
+    allPages,
+    lastPageParam
+  ) => {
+    if (((lastPage as SearchResponse).results || (lastPage as Image[])).length === 0) return;
+    return lastPageParam + 1;
+  };
+
   const { ref, inView, entry } = useInView();
   const { data, fetchNextPage, isFetchingNextPage, hasNextPage } = useInfiniteQuery({
     queryKey: ['photos'],
     queryFn: getPhotos,
-    select: (photo) => {
-      const photoWithLike = photo.pages.map((page) =>
-        ((page as SearchResponse).results || (page as Image[])).map((photo) => ({
-          ...photo,
-          isLiked: localStorage.getItem(String(photo.id)) === 'true',
-        }))
-      );
-
-      return {
-        pages: photoWithLike,
-        pageParams: photo.pageParams,
-      };
-    },
+    select: transformPhoto,
     initialPageParam: 1,
-    getNextPageParam: (lastPage, allPages, lastPageParam) => {
-      if (((lastPage as SearchResponse).results || (lastPage as Image[])).length === 0) return;
-      return lastPageParam + 1;
-    },
+    getNextPageParam,
     refetchOnWindowFocus: false,
   });
 
