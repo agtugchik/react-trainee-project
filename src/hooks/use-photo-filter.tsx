9651bb139ba498ba72b395c4778d204a04c90442
agtugchik <agtugchik13@gmail.com>
2024-12-26 19:33:55 +0300

    fix: refetching all queries

--- src/hooks/use-photo-filter.tsx
+++ src/hooks/use-photo-filter.tsx
@@ -1,4 +1,5 @@
 import { useQueryClient } from '@tanstack/react-query';
+import { Image } from 'types/unsplash-types';
 
 type UsePhotoFilterr = () => (name: string, value: string) => void;
 type ChangeFilter = (name: string, value: string) => void;
@@ -7,6 +8,10 @@ export const usePhotoFilter: UsePhotoFilterr = () => {
   const queryClient = useQueryClient();
   const changeFilter: ChangeFilter = (name, value) => {
     localStorage.setItem(name, value);
+    queryClient.setQueryData(['photos'], (data: { pageParams: number[]; pages: Image[][] }) => ({
+      pages: data.pages.length ? [data.pages[0]] : [],
+      pageParams: data.pageParams.length ? [data.pageParams[0]] : [],
+    }));
     queryClient.invalidateQueries({ queryKey: ['photos'] });
   };
 
